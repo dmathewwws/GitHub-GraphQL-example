eef4535434ba9e9df34172176de3adf17a8bd9fe
Daniel Mathews <dannyl.mathews@gmail.com>
2016-10-15 14:31:47 -0700

    Initial Commit

---
+++ GitHub-GraphQL-example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHub-GraphQL-example
+//
+//  Created by Admin on 2016-10-15.
+//  Copyright © 2016 ToyBox Media. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
